29419370e70f433284b73a6bff91d9c302ab210f
Gorazd Breskvar <gorazd.breskvar@whiletrue.com>
2025-07-05 15:37:30 +0200

    better bezier support

--- src/bh/paths/MultiAnimPaths.hx
+++ src/bh/paths/MultiAnimPaths.hx
@@ -11,6 +11,7 @@ using tweenxcore.Tools;
 enum PathType {
 	Checkpoint(name:String);
 	Line;
+	Arc(center:FPoint, startAngle:Float, radius:Float, angleDelta:Float);
 	Bezier2(control:FPoint);
 	Bezier3(control1:FPoint, control2:FPoint);
 }
@@ -68,7 +69,7 @@ class MultiAnimPaths {
 				case LineTo(end):
 					var end = resolveCoordinate(end);
 					singlePaths.push(new SinglePath(point, end, Line));
-					angle = hxd.Math.atan2(end.y - end.y, point.x - point.x);
+					angle = hxd.Math.atan2(end.y - point.y, end.x - point.x);
 					point = end;
 
 				case Forward(distance):
@@ -80,19 +81,49 @@ class MultiAnimPaths {
 					var angleDelta = resolveNumber(angleDelta);
 					angle = hxd.Math.angle(angle + hxd.Math.degToRad(angleDelta));
 
+				case Arc(radius, angleDelta):
+					var radius = resolveNumber(radius);
+					var angleDeltaF = resolveNumber(angleDelta);
+					var angleDeltaRad = hxd.Math.degToRad(angleDeltaF);
+
+					// Perpendicular direction: +90° for CCW, -90° for CW
+					var perpAngle = angle + (angleDeltaF > 0 ? Math.PI / 2 : -Math.PI / 2);
+					var centerX = point.x + radius * Math.cos(perpAngle);
+					var centerY = point.y + radius * Math.sin(perpAngle);
+					var center = new FPoint(centerX, centerY);
+
+					// Start angle for the arc (from center to start point)
+					var startAngle = Math.atan2(point.y - centerY, point.x - centerX);
+					var endAngle = startAngle + angleDeltaRad;
+
+					var end = new FPoint(
+						centerX + radius * Math.cos(endAngle),
+						centerY + radius * Math.sin(endAngle)
+					);
+					singlePaths.push(new SinglePath(point, end, Arc(center, startAngle, radius, angleDeltaF)));
+
+					// Update direction for next segment
+					angle += angleDeltaRad;
+					point = end;
+
 				case Checkpoint(name):
 					singlePaths.push(new SinglePath(point, point, Checkpoint(name)));
 
 				case Bezier2To(end, control):
 					var end = resolveCoordinate(end);
-					singlePaths.push(new SinglePath(point, end, Bezier2(resolveCoordinate(control))));
-					angle = hxd.Math.atan2(end.y - end.y, point.x - point.x);
+					var control = resolveCoordinate(control);
+					singlePaths.push(new SinglePath(point, end, Bezier2(control)));
+					// For quadratic bezier, the tangent at the end point is from control point to end point
+					angle = hxd.Math.atan2(end.y - control.y, end.x - control.x);
 					point = end;
 
 				case Bezier3To(end, control1, control2):
 					var end = resolveCoordinate(end);
-					singlePaths.push(new SinglePath(point, end, Bezier3(resolveCoordinate(control1), resolveCoordinate(control2))));
-					angle = hxd.Math.atan2(end.y - end.y, point.x - point.x);
+					var control1 = resolveCoordinate(control1);
+					var control2 = resolveCoordinate(control2);
+					singlePaths.push(new SinglePath(point, end, Bezier3(control1, control2)));
+					// For cubic bezier, the tangent at the end point is from control2 to end point
+					angle = hxd.Math.atan2(end.y - control2.y, end.x - control2.x);
 					point = end;
 			}
 		}
@@ -197,6 +228,13 @@ private class SinglePath {
 				return start.clone();
 			case Line:
 				return new FPoint(rate.lerp(start.x, end.x), rate.lerp(start.y, end.y));
+			case Arc(center, startAngle, radius, angleDelta):
+				var angleDeltaRad = hxd.Math.degToRad(angleDelta);
+				var currentAngle = startAngle + angleDeltaRad * rate;
+				return new FPoint(
+					center.x + radius * Math.cos(currentAngle),
+					center.y + radius * Math.sin(currentAngle)
+				);
 			case Bezier2(control1):
 				return new FPoint(rate.bezier2(start.x, control1.x, end.x),
 					rate.bezier2(start.y, control1.y, end.y));
@@ -210,6 +248,12 @@ private class SinglePath {
 		return switch path {
 			case Line: 
 				[start, end];
+			case Arc(center, startAngle, radius, angleDelta):
+				var arcLength = length();
+				// Calculate steps based on arc length: approximately 1 point per pixel
+				// For very small arcs, ensure at least 3 points for smooth rendering
+				var steps = hxd.Math.imax(3, cast arcLength);
+				[for(i in 0...steps) getPoint(1.0*i/steps)];
 			default:
 				var steps = hxd.Math.imax(2, cast length() / 4);
 				[for(i in 0...steps) getPoint(1.0*i/steps)];
@@ -232,6 +276,9 @@ private class SinglePath {
 		return switch path {
 			case Line:
 				hxd.Math.distance(start.x - end.x, start.y - end.y);
+			case Arc(center, startAngle, radius, angleDelta):
+				var angleDeltaRad = hxd.Math.degToRad(angleDelta);
+				radius * Math.abs(angleDeltaRad);
 			case Bezier2(control):
 				estimate(4);
 			case Bezier3(control1, control2):
